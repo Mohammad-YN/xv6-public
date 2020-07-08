758c4720974c4b19c571f784f8a392edbbece934
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2020-07-08 20:31:02 +0430

    Add new systemcall number to syscall.h

--- syscall.h
+++ syscall.h
@@ -21,3 +21,4 @@
 #define SYS_mkdir  20
 #define SYS_close  21
 #define SYS_pinfo  22  // Assigning a new number for our new systemcall
+#define SYS_waitx  23  // Assigning a new number for our new waitx systemcall
